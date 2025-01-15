5a1515b9c6aaaafc278968af5c00e031b4620b34
Erick Willyan <erickwillyan2105@gmail.com>
2025-01-15 17:59:46 -0300

    form completed

--- formularios/src/App.tsx
+++ formularios/src/App.tsx
@@ -1,28 +1,38 @@
 import { useState, useRef, FormEvent } from "react";
 import styles from "./App.module.css";
+import FuncionarioBox from "./components/FuncionarioBox";
 
 interface FuncionarioProps {
+  readonly id: string | number;
   name: string;
   cargo: string;
 }
 function App() {
   const nameRef = useRef<HTMLInputElement>(null);
   const cargoRef = useRef<HTMLInputElement>(null);
+  const idRef = useRef<HTMLInputElement>(null);
   const [funcionarios, setFuncionario] = useState<FuncionarioProps[]>([]);
 
   function HandleRegister(event: FormEvent) {
     event?.preventDefault();
-    if (!nameRef.current?.value || !cargoRef.current?.value) {
+    if (
+      !nameRef.current?.value ||
+      !cargoRef.current?.value ||
+      !idRef.current?.value
+    ) {
       alert("Preencha todos os campos!!!");
       return;
     }
+
     let user = {
+      id: idRef.current?.value,
       name: nameRef.current?.value,
       cargo: cargoRef.current?.value,
     };
 
     setFuncionario((values) => [...values, user]);
 
+    idRef.current.value = "";
     nameRef.current.value = "";
     cargoRef.current.value = "";
   }
@@ -30,15 +40,22 @@ function App() {
   return (
     <>
       <form className={styles.form} onSubmit={HandleRegister}>
+        <h1 className={styles.title}>Registro de funcionários</h1>
         <input
           className={styles.textbox}
-          placeholder="DIgite seu nome:"
+          placeholder="DIgite o id do funcionário:"
+          type="text"
+          ref={idRef}
+        />
+        <input
+          className={styles.textbox}
+          placeholder="DIgite o nome do funcionário:"
           type="text"
           ref={nameRef}
         />
         <input
           className={styles.textbox}
-          placeholder="DIgite seu cargo:"
+          placeholder="DIgite o cargo:"
           type="text"
           ref={cargoRef}
         />
@@ -46,10 +63,12 @@ function App() {
       </form>
       <div className={styles.components}>
         {funcionarios.map((funcionarios: FuncionarioProps) => (
-          <div key={funcionarios.name}>
-            <p>{funcionarios.name}</p>
-            <p>{funcionarios.cargo}</p>
-          </div>
+          <FuncionarioBox
+            key={funcionarios.id}
+            id={funcionarios.id}
+            name={funcionarios.name}
+            cargo={funcionarios.cargo}
+          />
         ))}
       </div>
     </>
--- formularios/src/components/FuncionarioBox/index.tsx
+++ formularios/src/components/FuncionarioBox/index.tsx
@@ -1,7 +1,30 @@
-export default function FuncionarioBox(name: string) {
+import style from "./style.module.css";
+
+interface FuncionarioProps {
+  readonly id: string | number;
+  name: string;
+  cargo: string;
+}
+
+export default function FuncionarioBox({ id, name, cargo }: FuncionarioProps) {
   return (
     <>
-      <p>Funcionário{name}</p>
+      <div className={style.container}>
+        <div className={style.infoBox}>
+          <p className={style.funcao}>Id do funcionário: </p>
+          <p className={style.dado}>{id}</p>
+        </div>
+
+        <div className={style.infoBox}>
+          <p className={style.funcao}>Funcionário: </p>
+          <p className={style.dado}>{name}</p>
+        </div>
+
+        <div className={style.infoBox}>
+          <p className={style.funcao}>Cargo: </p>
+          <p className={style.dado}>{cargo}</p>
+        </div>
+      </div>
     </>
   );
 }
